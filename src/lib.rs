84aeca1844bfb985303916b7e537d4ecc19262b2
Silvan Buedenbender <silvancodes@gmail.com>
2023-04-23 18:49:08 +0200

    Expose common structs and traits on top level for easier access

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,21 @@ pub mod neat_original;
 pub mod network;
 pub mod sparse_matrix;
 
+pub use matrix::{
+    feedforward::{evaluator::MatrixFeedforwardEvaluator, fabricator::MatrixFeedforwardFabricator},
+    recurrent::{evaluator::MatrixRecurrentEvaluator, fabricator::MatrixRecurrentFabricator},
+};
+
+pub use sparse_matrix::{
+    feedforward::{
+        evaluator::SparseMatrixFeedforwardEvaluator, fabricator::SparseMatrixFeedforwardFabricator,
+    },
+    recurrent::{
+        evaluator::SparseMatrixRecurrentEvaluator, fabricator::SparseMatrixRecurrentFabricator,
+    },
+};
+
+pub use network::{Evaluator, Fabricator, StatefulEvaluator, StatefulFabricator};
+
 type Matrix = Vec<Vec<f64>>;
 type Transformations = Vec<fn(f64) -> f64>;
